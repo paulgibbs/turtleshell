4b1c899bd450fb5b503c249bf4859e8beb21cbaf
Paul Gibbs <paul@byotos.com>
2012-09-29 19:11:18 +0100

    Don't treat index.php as a wrapper. Props karmatosed

--- buddypress/members/index.php
+++ buddypress/members/index.php
@@ -1,9 +1,34 @@
 <?php
 /**
- * Members directory. Backpat template.
+ * Archive Member content part
  *
  * @package BuddyPress
  * @subpackage TurtleShell
  */
+?>
 
-bp_get_template_part( 'members/content', 'archive-member' );
+<?php do_action( 'bp_before_directory_members_page' ); ?>
+
+<div id="buddypress">
+
+	<?php do_action( 'bp_before_directory_members' ); ?>
+
+	<?php if ( bp_has_members() ) : ?>
+
+		<?php bp_get_template_part( 'members/pagination', 'members' ); ?>
+
+		<?php bp_get_template_part( 'members/loop', 'members' ); ?>
+
+		<?php bp_get_template_part( 'members/pagination', 'members' ); ?>
+
+	<?php else : ?>
+
+		<?php bp_get_template_part( 'members/feedback', 'no-members' ); ?>
+
+	<?php endif; ?>
+
+	<?php do_action( 'bp_after_directory_members' ); ?>
+
+</div><!-- #buddypress -->
+
+<?php do_action( 'bp_after_directory_members_page' ); ?>
