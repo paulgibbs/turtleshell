ceed79e3e1d20a5bd1aa53d52e065f3be77f14ed
Paul Gibbs <paul@byotos.com>
2012-09-29 19:02:03 +0100

    Port of karmatosed's CSS/JS

--- buddypress/members/loop-single-member.php
+++ buddypress/members/loop-single-member.php
@@ -10,23 +10,26 @@
 <li id="bp-member-<?php bp_member_user_id(); ?>" <?php //@todo: bp_member_class(); ?>>
 	<?php do_action( 'bp_template_in_members_loop_early' ); ?>
 
-
-	<?php do_action( 'bp_template_before_member_avatar' ); ?>
-
-	<a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar(); ?></a>
-
-	<?php do_action( 'bp_template_after_member_avatar' ); ?>
-
-
-	<?php do_action( 'bp_template_before_member_name' ); ?>
-
-	<a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
-
-	<?php do_action( 'bp_template_after_member_name' ); ?>
-
-
-	<?php do_action( 'bp_directory_members_item' );     // Backpat ?>
-	<?php do_action( 'bp_directory_members_actions' );  // Backpat ?>
+			<div class="member-single">
+				<div class="member-avatar avatar">
+					<a href="<?php bp_member_permalink(); ?>"><?php bp_member_avatar(); ?></a>
+				</div>
+				<div class="member-title item-title">
+					<a href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a>
+				</div>
+				<div class="member-details item-details">
+					<?php if ( bp_get_member_latest_update() ) : ?>
+						<span class="member-update update"> <?php bp_member_latest_update(); ?></span>
+					<?php endif; ?>
+				<div class="member-meta item-meta">
+					<span class="-member-activity activity"><?php bp_member_last_active(); ?></span>
+				</div>
+				<?php do_action( 'bp_directory_members_item' ); ?>
+				</div>
+				<div class="member-action item-action">
+					<?php do_action( 'bp_directory_members_actions' ); ?>
+				</div>
+			</div>
 
 	<?php do_action( 'bp_template_in_members_loop_late' ); ?>
 </li>
