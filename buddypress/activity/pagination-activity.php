da167f84502ca26454b4133cdadbfb3c5ccfa9ca
karmatosed <karmatosed@gmail.com>
2012-10-01 21:32:29 +0100

    Make pagination have bp-pagination class
    Signed-off-by: karmatosed <karmatosed@gmail.com>

--- buddypress/activity/pagination-activity.php
+++ buddypress/activity/pagination-activity.php
@@ -1,7 +1,7 @@
 <?php /* Show pagination if JS is not enabled, since the "Load More" link will do nothing */ ?>
 	<noscript>
-		<div class="pagination">
-			<div class="pag-count"><?php bp_activity_pagination_count(); ?></div>
-			<div class="pagination-links"><?php bp_activity_pagination_links(); ?></div>
+		<div class="bp-pagination">
+			<div class="bp-pagination-count"><?php bp_activity_pagination_count(); ?></div>
+			<div class="bp-pagination-links"><?php bp_activity_pagination_links(); ?></div>
 		</div>
 	</noscript>
