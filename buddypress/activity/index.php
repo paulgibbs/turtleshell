e088de71f6158f99068bc915c7e0d31bdf7cd9c7
Paul Gibbs <paul@byotos.com>
2012-10-01 22:14:23 +0100

    Remove parts not on activity directory wireframe

--- buddypress/activity/index.php
+++ buddypress/activity/index.php
@@ -9,11 +9,7 @@
 <?php do_action( 'bp_before_directory_activity' ); ?>
 <div id="buddypress">
 	<?php do_action( 'bp_before_directory_activity_content' ); ?>
-	<?php if ( is_user_logged_in() ) : ?>
-		<?php //bp_get_template_part( 'activity/post-form' ); ?>
-	<?php endif; ?>
 	<?php do_action( 'template_notices' ); ?>
-	<?php // this will be broked bp_get_template_part( 'activity/filter', 'activity' ); ?>
 	<?php do_action( 'bp_before_directory_activity_list' ); ?>
 	<div class="bp-activity">
 		<?php bp_get_template_part( 'activity/loop', 'activity' ); ?>
