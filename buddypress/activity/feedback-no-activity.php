5ab3f12936c173d9e5bc013d1e1fc2cafe99af82
karmatosed <karmatosed@gmail.com>
2012-10-01 21:35:35 +0100

    Update nothing found feedback messages
    Signed-off-by: karmatosed <karmatosed@gmail.com>

--- buddypress/activity/feedback-no-activity.php
+++ buddypress/activity/feedback-no-activity.php
@@ -1,3 +1,3 @@
-<div id="message" class="info">
+<div id="bp-message" class="activity">
 	<p><?php _e( 'Sorry, there was no activity found. Please try a different filter.', 'buddypress' ); ?></p>
 </div>
