1932e120eecafcf2f7014053f5f1650c8729053b
Paul Gibbs <paul@byotos.com>
2012-10-03 19:57:54 +0100

    Use new bp_blog_class() function in BP trunk on the site directory template loop

--- buddypress/blogs/loop-single-blog.php
+++ buddypress/blogs/loop-single-blog.php
@@ -7,7 +7,7 @@
  */
 ?>
 
-<li id="bp-blog-<?php bp_blog_id(); ?>" <?php //@todo: bp_blog_class(); ?>>
+<li id="bp-blog-<?php bp_blog_id(); ?>" <?php bp_blog_class(); ?>>
 	<?php do_action( 'bp_template_in_blogs_loop_early' ); ?>
 
 
