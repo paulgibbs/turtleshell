54e5f25e327645be0dc3e46ad05ef3bd3ef46a96
karmatosed <karmatosed@gmail.com>
2012-10-04 20:06:46 +0100

    Activity item styling
    Basic styling
    Note : Need to work out if we are going to change the date to be separate as in wireframes or keep as is in this one header output.
    
    Signed-off-by: karmatosed <karmatosed@gmail.com>

--- buddypress/activity/loop-single-activity.php
+++ buddypress/activity/loop-single-activity.php
@@ -10,7 +10,6 @@
 <li id="bp-activity-<?php bp_activity_id(); ?>" <?php bp_activity_css_class(); ?>>
 	<?php do_action( 'bp_template_in_activity_loop_early' ); ?>
 
-
 	<div class="activity-avatar">
 		<a href="<?php bp_activity_user_link(); ?>">
 			<?php bp_activity_avatar(); ?>
@@ -24,9 +23,9 @@
 		</div>
 
 		<?php if ( 'activity_comment' == bp_get_activity_type() ) : ?>
-			<div class="activity-inreplyto">
-				<strong><?php _e( 'In reply to: ', 'buddypress' ); ?></strong><?php bp_activity_parent_content(); ?> <a href="<?php bp_activity_thread_permalink(); ?>" class="view" title="<?php _e( 'View Thread / Permalink', 'buddypress' ); ?>"><?php _e( 'View', 'buddypress' ); ?></a>
-			</div>
+			<span class="activity-inreply">
+				<?php _e( 'In reply to: ', 'buddypress' ); ?></strong><?php bp_activity_parent_content(); ?> <a href="<?php bp_activity_thread_permalink(); ?>" class="view" title="<?php _e( 'View Thread / Permalink', 'buddypress' ); ?>"><?php _e( 'View', 'buddypress' ); ?></a>
+			</span>
 		<?php endif; ?>
 
 		<?php if ( bp_activity_has_content() ) : ?>
@@ -38,18 +37,27 @@
 		<?php if ( is_user_logged_in() ) : ?>
 			<div class="activity-meta">
 				<?php if ( bp_activity_can_comment() ) : ?>
-					<a href="<?php bp_get_activity_comment_link(); ?>" class="button acomment-reply bp-primary-action" id="acomment-comment-<?php bp_activity_id(); ?>"><?php printf( __( 'Comment <span>%s</span>', 'buddypress' ), bp_activity_get_comment_count() ); ?></a>
+					<span class="activity-comment">
+						<a href="<?php bp_get_activity_comment_link(); ?>" class="button acomment-reply bp-primary-action" id="acomment-comment-<?php bp_activity_id(); ?>"><?php printf( __( 'Comment <span>%s</span>', 'buddypress' ), bp_activity_get_comment_count() ); ?></a>
+					</span>
 				<?php endif; ?>
 
 				<?php if ( bp_activity_can_favorite() ) : ?>
 					<?php if ( !bp_get_activity_is_favorite() ) : ?>
+					<span class="activity-favorite">
 						<a href="<?php bp_activity_favorite_link(); ?>" class="button fav bp-secondary-action" title="<?php esc_attr_e( 'Mark as Favorite', 'buddypress' ); ?>"><?php _e( 'Favorite', 'buddypress' ); ?></a>
+					</span>
 					<?php else : ?>
+					<span class="activity-unfavorite">
 						<a href="<?php bp_activity_unfavorite_link(); ?>" class="button unfav bp-secondary-action" title="<?php esc_attr_e( 'Remove Favorite', 'buddypress' ); ?>"><?php _e( 'Remove Favorite', 'buddypress' ); ?></a>
+					</span>
 					<?php endif; ?>
 				<?php endif; ?>
-
-				<?php if ( bp_activity_user_can_delete() ) bp_activity_delete_link(); ?>
+				<?php if ( bp_activity_user_can_delete() ) : ?>
+					<span class="activity-delete">
+						<?php bp_activity_delete_link(); ?>
+					</span>
+				<?php endif; ?>
 			</div>
 		<?php endif; ?>
 
