f18bb54fd57b26f48828748f8341c665afe33d38
Paul Gibbs <paul@byotos.com>
2012-10-14 15:06:24 +0100

    Remove user name from member menus

--- buddypress/members/single/menu-members.php
+++ buddypress/members/single/menu-members.php
@@ -12,9 +12,5 @@
 		<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_avatar( 'type=full' ); ?></a>
 	</li>
 
-	<li class="member-menu-user">
-		<?php bp_displayed_user_fullname(); ?>
-	</li>
-
 	<?php ts_bp_member_menu(); ?>
 </ul>
