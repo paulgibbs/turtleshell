e4bdaa8395e774fe66072fce034c030c1e9171f8
Paul Gibbs <paul@byotos.com>
2012-10-14 11:27:01 +0100

    Add username to member menu.

--- buddypress/members/single/menu-members.php
+++ buddypress/members/single/menu-members.php
@@ -8,8 +8,12 @@
 ?>
 
 <ul id="bp-member-menu">
-	<li class="member-menu-user">
+	<li class="member-menu-avatar">
 		<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_avatar( 'type=full' ); ?></a>
+	</li>
+
+	<li class="member-menu-user">
+		<?php bp_displayed_user_fullname(); ?>
 	</li>
 
 	<?php ts_bp_member_menu(); ?>
