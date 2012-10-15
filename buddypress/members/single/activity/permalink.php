37ea6171c02c60f260e3db7d8ce33b3d35d2d7c3
David Carson <davidtcarson@gmail.com>
2012-10-15 17:31:57 -0400

    cleanup hooks

--- buddypress/members/single/activity/permalink.php
+++ buddypress/members/single/activity/permalink.php
@@ -7,16 +7,18 @@
  */
 ?>
 
-<?php do_action( 'bp_before_activity_entry' ); ?>
-
 <div id="buddypress">
 
 	<?php if ( bp_has_activities() ) : ?>
 
 		<ul class="bp-archive-activity">
 		<?php while ( bp_activities() ) : bp_the_activity(); ?>
+		
+			<?php do_action( 'bp_before_activity_entry' ); ?>
 
 			<?php bp_get_template_part( 'activity/loop', 'single-activity' ); ?>
+			
+			<?php do_action( 'bp_after_activity_entry' ); ?>
 		
 		<?php endwhile; ?>
 		</ul>
@@ -24,3 +26,4 @@
 	<?php endif; ?>
 
 </div><!-- #buddypress -->
+
