87a3f89f7ca2ba1a0ef44b21f032341deda6fc9c
r-a-y <r-a-y@gmx.com>
2012-10-17 21:50:51 -0700

    Add member plugins template.

---
+++ buddypress/members/single/plugins.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Single member plugins template
+ *
+ * @package BuddyPress
+ * @subpackage TurtleShell
+ */
+?>
+
+<?php do_action( 'bp_before_member_plugin_template' ); ?>
+
+<?php do_action( 'bp_template_content' ); ?>
+
+<?php do_action( 'bp_after_member_plugin_template' ); ?>
