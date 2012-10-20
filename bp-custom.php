13970491bbf32ecaf6596791f119f6c6372f4777
Paul Gibbs <paul@byotos.com>
2012-10-20 18:19:47 +0100

    UPDATE YOUR BP-CUSTOM.PHP. Stops groups/members widget JS being enqueued.

--- bp-custom.php
+++ bp-custom.php
@@ -17,5 +17,11 @@ function turtlepower_package_id( $package_id ) {
 }
 add_filter( 'pre_option__bp_theme_package_id', 'turtlepower_package_id' );
 
+function turtlepower_kill_legacy_js_and_css() {
+	wp_dequeue_script( 'groups_widget_groups_list-js' );
+	wp_dequeue_script( 'bp_core_widget_members-js' );
+}
+add_action( 'wp', 'turtlepower_kill_legacy_js_and_css', 999 );
+
 if ( ! defined( 'BP_DEFAULT_COMPONENT' ) )
 	define( 'BP_DEFAULT_COMPONENT', 'profile' );
