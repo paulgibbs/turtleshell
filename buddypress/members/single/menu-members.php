967bae01c8f40922a5e5b4b79530a6fad359ce3c
Paul Gibbs <paul@byotos.com>
2012-10-20 15:13:08 +0100

    Add friend button to member menu. Also remove unused CSS from older version. See #9

--- buddypress/members/single/menu-members.php
+++ buddypress/members/single/menu-members.php
@@ -13,6 +13,12 @@
 			<a href="<?php bp_displayed_user_link(); ?>"><?php bp_displayed_user_avatar( 'type=full' ); ?></a>
 		</li>
 
+		<?php if ( ! bp_is_my_profile() ) : ?>
+			<li class="member-menu-friend">
+				<?php bp_add_friend_button(); ?>
+			</li>
+		<?php endif; ?>
+
 		<?php bp_get_displayed_user_nav(); ?>
 	</ul>
 </div><!-- #bp-member-menu -->
