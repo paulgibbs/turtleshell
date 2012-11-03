217bd19077fd1cfc2583c7ab194a4e96177d8c98
Paul Gibbs <paul@byotos.com>
2012-11-03 13:23:00 +0000

    Rework members/groups search forms to match activity search form structure.
    * Clears a float.
    * Moves form elements into a class container; prevents our generic
    styles overriding any form elements that other plugins/themes might
    have added.
    * Fixes #18

--- buddypress/members/form-members-search.php
+++ buddypress/members/form-members-search.php
@@ -9,14 +9,14 @@
 
 <?php do_action( 'bp_template_before_member_search_form' ); ?>
 
-<form action="" id="bp_member_search" method="get">
+<form action="" id="bp_member_search_form" method="get">
 
 	<?php do_action( 'bp_template_member_search_form_extras_top' ); ?>
 
-	<div>
-		<label for="bp_member_search_box"><?php _e( 'Discover people in the community and make connections with new friends', 'buddypress' ); ?></label>
+	<div class="searchwrapper">
+		<label for="bp_member_search_form_box"><?php _e( 'Discover people in the community and make connections with new friends', 'buddypress' ); ?></label>
 		<input class="submit" type="submit" value="<?php esc_attr_e( 'Search', 'buddypress' ) ?>" />
-		<span><input class="search" id="bp_member_search_box" type="search" name="s" placeholder="<?php echo esc_attr( bp_get_search_default_text( 'members' ) ); ?>" value="<?php echo esc_attr( ! empty( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '' ); ?>" required autofocus /></span>
+		<span><input class="search" id="bp_member_search_form_box" type="search" name="s" placeholder="<?php echo esc_attr( bp_get_search_default_text( 'members' ) ); ?>" value="<?php echo esc_attr( ! empty( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '' ); ?>" required autofocus /></span>
 	</div>
 
 	<?php do_action( 'bp_template_member_search_form_extras_bottom' ); ?>
