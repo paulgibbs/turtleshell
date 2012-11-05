733599f03a0b62ce1d6b3326715ec61f670e4d23
Paul Gibbs <paul.gibbs@telegraph.co.uk>
2012-11-05 12:45:39 +0000

    Tweak label text on group directory

--- buddypress/groups/form-groups-search.php
+++ buddypress/groups/form-groups-search.php
@@ -14,7 +14,7 @@
 	<?php do_action( 'bp_template_group_search_form_extras_top' ); ?>
 
 	<div class="searchwrapper">
-		<label for="bp_group_search_form_box"><?php _e( 'Find groups and interact with like-minded people that share a common interest', 'buddypress' ); ?></label>
+		<label for="bp_group_search_form_box"><?php _e( 'Find groups and interact with like-minded people who share a common interest', 'buddypress' ); ?></label>
 		<input class="submit" type="submit" value="<?php esc_attr_e( 'Search', 'buddypress' ) ?>" />
 		<span><input class="search" id="bp_group_search_form_box" type="search" name="s" placeholder="<?php echo esc_attr( bp_get_search_default_text( 'groups' ) ); ?>" value="<?php echo esc_attr( ! empty( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '' ); ?>" required autofocus /></span>
 	</div>
