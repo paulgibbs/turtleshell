9de7bd5be3432e788c511346d8e8a0fb4b0ab521
Paul Gibbs <paul@byotos.com>
2012-11-05 19:33:51 +0000

    Link no-friend feedback text to members directory

--- buddypress/members/single/friends/feedback-no-friend-requests.php
+++ buddypress/members/single/friends/feedback-no-friend-requests.php
@@ -8,5 +8,5 @@
 ?>
 
 <div class="bp-template-notice info friend-requests">
-	<p><?php _e( 'No-one wants to be your friend! You should get involved with the community and meet new people.', 'buddypress' ); ?></p>
+	<p><?php printf( __( 'No-one wants to be your friend! You should get involved with the community and <a href="%s">meet new people</a>.', 'buddypress' ), esc_url( bp_get_members_directory_permalink() ) ); ?></p>
 </div>
--- buddypress/members/single/friends/feedback-no-friends.php
+++ buddypress/members/single/friends/feedback-no-friends.php
@@ -9,7 +9,7 @@
 
 <?php if ( bp_is_my_profile() ) : ?>
 	<div class="bp-template-notice info friends">
-		<p><?php printf( __( 'You have no friends! Get involved with the community and meet people.', 'buddypress' ), bp_get_displayed_user_fullname() ); ?></p>
+		<p><?php printf( __( 'You have no friends! Get involved with the community and <a href="%s">meet people</a>.', 'buddypress' ), esc_url( bp_get_members_directory_permalink() ) ); ?></p>
 	</div>
 
 <?php else : ?>
