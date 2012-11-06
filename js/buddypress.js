45af751788f0911fca2b6d2dbc2586969d82349e
Paul Gibbs <paul@byotos.com>
2012-11-06 11:53:54 +0000

    Make member menu accessible. Fixes #30
    * Implemented via javascript
    * Props to Graham Armfield for advice and JavaScript approach.
    * Refactored the JS file again; this should now be a bit more flexible
    for people wanting to affect core javascript.

--- js/buddypress.js
+++ js/buddypress.js
@@ -1,9 +1,26 @@
 /*! http://buddypress.org/ */
 (function($) {
 
+// Options
+var bp_options = $.extend({
+	menu_accessibility: true,
+	menu_hoverIntent:   true
+}, bp_options);
+
+// Functions
 var buddypress = {
-	setup_menu: function() {
-		// Prevent CSS :hover
+
+	/**
+	 * Set up hoverIntent for the navigation menu
+	 *
+	 * @since BuddyPress (1.7)
+	 */
+	menu: function() {
+		if (true !== bp_options.menu_hoverIntent) {
+			return;
+		}
+
+		// Disable CSS :hover
 		$('#bp-menu .bp-navigation').removeClass('no-menu-js');
 
 		// Use hoverIntent to re-implement :hover
@@ -14,17 +31,45 @@ var buddypress = {
 			out: function(){
 				$(this).find('> .sub-menu').removeClass('bp-show-menu');
 			},
-
-			timeout: 180,
+			timeout:     180,
 			sensitivity: 7,
-			interval: 100
+			interval:    100
+		});
+	},
+
+	/**
+	 * Makes the navigation menu more accessible to keyboard browsers
+	 *
+	 * @since BuddyPress (1.7)
+	 */
+	menu_accessibility: function() {
+		if (true !== bp_options.menu_accessibility) {
+			return;
+		}
+
+		$('#menu-bp').on('focus.bp_menu_accessibility', 'li a', function() {
+			$(this).parent().find('> .sub-menu').addClass('bp-show-menu');
+
+			// If this is a nested menu, we need to show the parent menu
+			if ( $(this).parent().parent().hasClass('sub-menu') ) {
+				$(this).parent().parent().addClass('bp-show-menu');
+			}
+		});
+
+		$('#menu-bp').on('blur.bp_menu_accessibility', 'li a', function() {
+			$(this).parent().find('> .sub-menu').removeClass('bp-show-menu');
+
+			// If this is a nested menu, we need to show the parent menu
+			if ( $(this).parent().parent().hasClass('sub-menu') ) {
+				$(this).parent().parent().removeClass('bp-show-menu');
+			}
 		});
 	}
 };
 
-
-$(document).ready(function(){
-	buddypress.setup_menu();
+$(document).ready( function() {
+	buddypress.menu();
+//	buddypress.menu_accessibility();
 });
 
 })(jQuery);
