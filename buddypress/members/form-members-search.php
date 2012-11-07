29b231fee19de8a4855163e6ae63c560e911975d
Paul Gibbs <paul@byotos.com>
2012-11-07 07:16:10 +0000

    Remove required attribute from Groups + Members search forms
    Some people may search with no query to clear a search.

--- buddypress/members/form-members-search.php
+++ buddypress/members/form-members-search.php
@@ -16,7 +16,7 @@
 	<div class="searchwrapper">
 		<label for="bp_member_search_form_box"><?php _e( 'Discover people in the community and make connections with new friends', 'buddypress' ); ?></label>
 		<input class="submit" type="submit" value="<?php esc_attr_e( 'Search', 'buddypress' ) ?>" />
-		<span><input class="search" id="bp_member_search_form_box" type="search" name="s" placeholder="<?php echo esc_attr( bp_get_search_default_text( 'members' ) ); ?>" value="<?php echo esc_attr( ! empty( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '' ); ?>" required autofocus /></span>
+		<span><input class="search" id="bp_member_search_form_box" type="search" name="s" placeholder="<?php echo esc_attr( bp_get_search_default_text( 'members' ) ); ?>" value="<?php echo esc_attr( ! empty( $_REQUEST['s'] ) ? stripslashes( $_REQUEST['s'] ) : '' ); ?>" autofocus /></span>
 	</div>
 
 	<?php do_action( 'bp_template_member_search_form_extras_bottom' ); ?>
