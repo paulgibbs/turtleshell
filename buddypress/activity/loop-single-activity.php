a0415301d674c0551a24482ea1a6c65bb2c19e41
Paul Gibbs <paul.gibbs@telegraph.co.uk>
2012-11-13 15:29:39 +0000

    Always use thumbnail avatar on activity permalink page

--- buddypress/activity/loop-single-activity.php
+++ buddypress/activity/loop-single-activity.php
@@ -15,7 +15,7 @@
 
 	<div class="activity-avatar">
 		<a href="<?php bp_activity_user_link(); ?>">
-			<?php bp_activity_avatar(); ?>
+			<?php bp_activity_avatar( 'type=thumb' ); ?>
 		</a>
 	</div>
 
