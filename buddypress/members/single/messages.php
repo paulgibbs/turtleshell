3d28651449211acd9802d95c83287d9a74ec61c3
Paul Gibbs <paul.gibbs@telegraph.co.uk>
2012-11-16 15:30:28 +0000

    Re-indent member profile > messages loop

--- buddypress/members/single/messages.php
+++ buddypress/members/single/messages.php
@@ -7,30 +7,30 @@
  */
 ?>
 
-	<?php do_action( 'bp_template_before_member_messages_content' ); ?>
+<?php do_action( 'bp_template_before_member_messages_content' ); ?>
 
-	<?php if ( bp_has_message_threads() ) : ?>
+<?php if ( bp_has_message_threads() ) : ?>
 
-		<?php		
-			if ( bp_is_current_action( 'compose' ) ) :
-				bp_get_template_part( 'members/single/messages/messages-compose', 'messages' );
+	<?php
+	if ( bp_is_current_action( 'compose' ) ) :
+		bp_get_template_part( 'members/single/messages/messages-compose' );
+
+	elseif ( bp_is_current_action( 'view' ) ) :
+		bp_get_template_part( 'members/single/messages/messages-single' );
 		
-			elseif ( bp_is_current_action( 'view' ) ) :
-				bp_get_template_part( 'members/single/messages/messages-single', 'messages' );
-				
-			elseif ( bp_is_current_action( 'notices' ) ) :
-				bp_get_template_part( 'members/single/messages/notices-loop', 'messages' );			
-			
-			else :
-				bp_get_template_part( 'members/single/messages/messages-loop', 'messages' );
+	elseif ( bp_is_current_action( 'notices' ) ) :
+		bp_get_template_part( 'members/single/messages/notices-loop' );
 	
-			endif; 
-		?>
-	
-	<?php else: ?>
-	
-		<?php bp_get_template_part( 'members/single/messages/no-messages', 'no-messages' ); ?>
+	else :
+		bp_get_template_part( 'members/single/messages/messages-loop' );
 
-	<?php endif;?>
-	
-	<?php do_action( 'bp_template_before_member_messages_content' ); ?>
+	endif; 
+	?>
+
+<?php else: ?>
+
+	<?php bp_get_template_part( 'members/single/messages/feedback-no-messages' ); ?>
+
+<?php endif;?>
+
+<?php do_action( 'bp_template_before_member_messages_content' ); ?>
