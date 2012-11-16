7ace46bec373cebed68d0852823acdfb659a431a
Paul Gibbs <paul.gibbs@telegraph.co.uk>
2012-11-16 15:36:09 +0000

    Update 'no messages found' notice to something more fun.

--- buddypress/members/single/messages/feedback-no-messages.php
+++ buddypress/members/single/messages/feedback-no-messages.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Messages no message content part
+ * No messages found template part
  *
  * @package BuddyPress
  * @subpackage TurtleShell
  */
 ?>
 
-<div id="message" class="info">
-	<p><?php _e( 'Sorry, no messages were found.', 'buddypress' ); ?></p>
+<div id="bp-template-notice info message" class="info">
+		<p><?php _e( 'Inbox at zero? Hip hip hooray!', 'buddypress' ); ?></p>
 </div>
