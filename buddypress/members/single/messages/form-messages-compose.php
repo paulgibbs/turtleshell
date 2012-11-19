ef44da5274187379530c7984f6d48ee5db73ed0e
Paul Gibbs <djpaul@gmail.com>
2012-11-19 12:30:17 +0000

    Tidy HTML in messages-compose template, and add actions.

--- buddypress/members/single/messages/form-messages-compose.php
+++ buddypress/members/single/messages/form-messages-compose.php
@@ -7,44 +7,45 @@
  */
 ?>
 
-<form action="<?php bp_messages_form_action('compose'); ?>" method="post" id="send_message_form" class="standard-form" role="main" enctype="multipart/form-data">
+<?php do_action( 'bp_template_before_member_messages_compose_form' ); ?>
 
-	<?php do_action( 'bp_template_before_messages_compose_content' ); ?>
+<form action="<?php bp_messages_form_action( 'compose' ); ?>" method="post" enctype="multipart/form-data">
 
-	<div class="message-to">
-		<label for="send-to-input"><?php _e("Send To (Username or Friend's Name)", 'buddypress'); ?></label>
-		
-		<ul class="first acfb-holder">
-		<li>
-			<?php bp_message_get_recipient_tabs(); ?>
-			<input type="text" name="send-to-input" class="send-to-input" id="send-to-input" autofocus />
-		</li>
+	<?php do_action( 'bp_template_member_messages_compose_form_extras_top' ); ?>
+
+	<fieldset>
+		<legend><?php _e( 'Message recipients', 'buddypress' ); ?></legend>
+
+		<label for="send-to-input"><?php _e( "Send To (Username or Friend's Name)", 'buddypress' ); ?></label>	
+		<ul>
+			<li>
+				<?php bp_message_get_recipient_tabs(); ?>
+				<input type="text" name="send-to-input" id="send-to-input" autofocus />
+			</li>
 		</ul>
-	</div><!-- .message-to -->
 
-	<?php if ( bp_current_user_can( 'bp_moderate' ) ) : ?>
-	<div class="message-all-notice">
-		<input type="checkbox" id="send-notice" name="send-notice" value="1" /> <?php _e( "This is a notice to all users.", "buddypress" ); ?>
-	</div>
-	<?php endif; ?>
-	
-	<div class="message-subject">
+		<?php if ( bp_current_user_can( 'bp_moderate' ) ) : ?>
+			<input type="checkbox" id="bp-send-notice" name="send-notice" value="1" /><?php _e( 'This is a notice to all users.', 'buddypress' ); ?>
+		<?php endif; ?>
+	</fieldset>
+
+	<fieldset>
+		<legend><?php _e( 'Message contents', 'buddypress' ); ?></legend>
+
 		<label for="subject"><?php _e( 'Subject', 'buddypress'); ?></label>
 		<input type="text" name="subject" id="subject" value="<?php bp_messages_subject_value(); ?>" />
-	</div><!-- .message-all-notice -->
-	
-	<div class="message-content">
+
 		<label for="content"><?php _e( 'Message', 'buddypress'); ?></label>
-		<textarea name="content" id="message_content" rows="15" cols="40"><?php bp_messages_content_value(); ?></textarea>
-	</div><!-- .message-content -->
+		<textarea name="content" id="message_content"><?php bp_messages_content_value(); ?></textarea>
+	</fieldset>
 
-	<input type="hidden" name="send_to_usernames" id="send-to-usernames" value="<?php bp_message_get_recipient_usernames(); ?>" class="<?php bp_message_get_recipient_usernames(); ?>" />
-
-	<?php do_action( 'bp_template_after_messages_compose_content' ); ?>
-
-	<div class="message-submit">
-		<input type="submit" value="<?php _e( "Send Message", 'buddypress' ); ?>" name="send" id="send" />
-	</div>
+	<input type="hidden" name="send_to_usernames" value="<?php echo esc_attr( bp_get_message_get_recipient_usernames() ); ?>" />
+	<input type="submit" value="<?php esc_attr_e( 'Send Message', 'buddypress' ); ?>" class="submit" />
 
 	<?php wp_nonce_field( 'messages_send_message' ); ?>
-</form><!-- #send-message-form -->
+
+	<?php do_action( 'bp_template_member_messages_compose_form_extras_bottom' ); ?>
+
+</form>
+
+<?php do_action( 'bp_template_after_member_messages_compose_form' ); ?>
